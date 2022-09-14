c372dc07b2ab2198d1ea721651e1ceb506dcbcc0
jendameli <melicherikjan84@gmail.com>
2022-09-14 15:38:56 +0200

    added user registration method, saved to DB + basic data validation

--- src/components/user/userController.js
+++ src/components/user/userController.js
@@ -1,3 +1,5 @@
+const userService = require("./userService");
+
 // TODO: Logic
 exports.getAllUsers = async (req, res) => {
   try {
@@ -14,11 +16,30 @@ exports.getUserById = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
-// TODO: Logic
+
+// Endpoint to create a new user registration
 exports.registerUser = async (req, res) => {
+  const userRegistrationData = req.body;
+  if (
+    !userRegistrationData.email ||
+    !userService.checkInputEmail(userRegistrationData.email)
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Email is required to finish your registration" });
+  }
   try {
-    return await res.status(200).json({ message: "Create user" });
+    await userService.registerUser(userRegistrationData);
+    return res
+      .status(200)
+      .json({ message: "User registration created successfully" });
   } catch (error) {
+    if (error.message === "Validation error") {
+      // TODO: redirect in FE to login page
+      return res
+        .status(200)
+        .json({ error: "This email is already registered" });
+    }
     return res.status(400).json({ error: error.message });
   }
 };
--- src/components/user/userService.js
+++ src/components/user/userService.js
@@ -1,3 +1,30 @@
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+
+const User = require("./userModel");
+
+// Helper functions
+const createRandomString = () => {
+  return crypto.randomBytes(12).toString("hex");
+};
+
+const hashPassword = (userPassword) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(userPassword, salt);
+  return hashedPassword;
+};
+
+const comparePasswords = (enteredPassword) => {
+  // TODO: Logic for compare passwords, boolean
+};
+
+// Check if user provided email is email
+exports.checkInputEmail = (userEmail) => {
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return userEmail.match(emailRegex);
+};
+
+// Main services for user resource
 // TODO: Logic
 exports.getAllUsers = async () => {
   return;
@@ -6,9 +33,19 @@ exports.getAllUsers = async () => {
 exports.getUserById = async () => {
   return;
 };
-// TODO: Logic
-exports.registerUser = async () => {
-  return;
+
+exports.registerUser = async (userData) => {
+  const hashedPassword = hashPassword(userData.password);
+  const registrationLink = createRandomString();
+  const userRegistration = Object.assign(userData, {
+    password: hashedPassword,
+    registrationHash: registrationLink,
+  });
+  try {
+    return await User.create(userRegistration);
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 // TODO: Logic
 exports.loginUser = async () => {
