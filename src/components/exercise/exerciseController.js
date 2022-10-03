cbcc65a7833fb472016f4e2fa866e47d720f94b2
smil3cz <janmelicherik@email.cz>
2022-10-03 17:25:58 +0200

    added new methods to exercise resource

--- src/components/exercise/exerciseController.js
+++ src/components/exercise/exerciseController.js
@@ -10,6 +10,19 @@ exports.getAllExercises = async (req, res) => {
   }
 };
 
+// Get all exercises from user
+exports.getExercisesByUser = async (req, res) => {
+  const userId = req.user.userId;
+  try {
+    const exercisesByUser = await exerciseService.getExercisesByUser(userId);
+    return res
+      .status(200)
+      .json({ count: exercisesByUser.length, data: exercisesByUser });
+  } catch (error) {
+    return res.status(400).json({ error: error.message || error });
+  }
+};
+
 // Create a new exercise
 exports.createExercise = async (req, res) => {
   const userData = req.body;
@@ -20,6 +33,30 @@ exports.createExercise = async (req, res) => {
     userData.userId = req.user.userId;
     await exerciseService.createExercise(userData);
     return res.status(200).json({ message: "Exercise created" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message || error });
+  }
+};
+
+// Get exercise by its PK
+exports.getExerciseById = async (req, res) => {
+  const { exerciseId } = req.params;
+  if (!exerciseId) {
+    return res.status(400).json({ message: "Missing exerciseId argument" });
+  }
+  try {
+    const exercise = await exerciseService.getExerciseById(exerciseId);
+    return res.status(200).json({ data: exercise });
+  } catch (error) {}
+};
+
+// Delete exercise
+exports.deleteExercise = async (req, res) => {
+  const { exerciseId } = req.params;
+  const { userId } = req.user;
+  try {
+    await exerciseService.deleteExercise(exerciseId, userId);
+    return res.sendStatus(204);
   } catch (error) {
     return res.status(400).json({ error: error.message || error });
   }
